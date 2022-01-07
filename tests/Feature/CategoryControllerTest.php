d609758a9e6776766041460d649590880f170fd5
Saeed <hovalkafi@gmail.com>
2022-01-08 00:04:53 +0330

    Category Controller Tests

--- tests/Feature/CategoryControllerTest.php
+++ tests/Feature/CategoryControllerTest.php
@@ -12,11 +12,43 @@ class CategoryControllerTest extends TestCase
 {
     use ViewAssertions;
 
+    /**
+     * index tests
+     */
+    public function test_indexMethod_MustLoad_pages_categories_index_View()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * addCategory tests
+     */
     public function test_addCategoryMethod_MustLoad_pages_categories_add_View()
     {
         $this->withoutMiddleware([Authenticate::class, GodUsers::class]);
         $this->expectViewFiles('pages.categories.add');
         $this->get('/category/add');
     }
+
+    /**
+     * editCategory tests
+     */
+
+
+    /**
+     * insertCategory tests
+     */
+
+
+    /**
+     * updateCategory tests
+     */
+
+
+    /**
+     * deleteCategory tests
+     */
+
+
 
 }
